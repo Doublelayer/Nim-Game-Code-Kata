85509b9d7432d1a98a009bfff8f5d3f76c8dcb7b
Doublelayer <florin.hamann@web.de>
2019-08-07 21:58:00 +0200

    refactoring gamecontroller to produce hateoas

--- src/main/java/de/hamann/florin/nimcodekata/nimcodekata/api/GameController.java
+++ src/main/java/de/hamann/florin/nimcodekata/nimcodekata/api/GameController.java
@@ -3,6 +3,7 @@ package de.hamann.florin.nimcodekata.nimcodekata.api;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.hamann.florin.nimcodekata.nimcodekata.model.Game;
@@ -21,6 +23,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 @RestController
 @RequestMapping("/api/v1/nim")
@@ -36,31 +40,45 @@ public class GameController {
 	public String welcome() {
 		return "Willkomen zum Nim-Game. <a href='http://localhost:8080/swagger-ui.html'>Hier finden Sie eine Dokumentation für diese API</a>";
 	}
+	
+	@PostMapping("/post")
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "create and save new init game to jpa repositry", response = ResponseEntity.class)
+	Resource<Game> newGame(@Valid @RequestBody Game newGame) {
 
-	@ApiOperation(value = "create and save new init game to jpa repositry", response = Game.class)
-	@ApiResponses(@ApiResponse(code = 201, message = "CREATED"))
-	@PostMapping(value = "/post")
-	ResponseEntity<Game> newGame(@Valid @RequestBody Game newGame) {
-		return new ResponseEntity<Game>(gameProviderService.createAndSaveNewGame(newGame), HttpStatus.CREATED);
+	  Game game = gameProviderService.createAndSaveNewGame(newGame);
+
+	  return new Resource<>(game,
+	    linkTo(methodOn(GameController.class).newGame(game)).withRel("_self"),
+	    linkTo(methodOn(GameController.class).playMove(new GameAction(), game.getGameId())).withRel("play_move"));
 	}
 
 	@GetMapping("/get/{id}")
-	@ApiOperation(value = "find and search stored game by given id", response = Game.class)
-	ResponseEntity<Game> find(@PathVariable Long id) {
-		return new ResponseEntity<Game>(gameProviderService.findGameById(id), HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "find and search stored game by given id", response = ResponseEntity.class)
+	Resource<Game> find(@PathVariable Long id) {
+		Game game = gameProviderService.findGameById(id);
+		return new Resource<Game>(game,
+				linkTo(methodOn(GameController.class).find(game.getGameId())).withRel("_self"),
+				linkTo(methodOn(GameController.class).playMove(new GameAction(), game.getGameId())).withRel("play_move"));
 	}
 
 	@PutMapping("/play/{id}")
-	@ApiOperation(value = "find, search and update stored game by given gameAction", response = Game.class)
-	ResponseEntity<Game> playMove(@RequestBody @Valid GameAction gameAction, @PathVariable Long id) {
-		return new ResponseEntity<Game>(gameProviderService.playMove(gameAction, id), HttpStatus.OK);
+	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "find, search and update stored game by given gameAction", response = ResponseEntity.class)
+	Resource<Game> playMove(@RequestBody @Valid GameAction gameAction, @PathVariable Long id) {
+		Game game = gameProviderService.playMove(gameAction, id);
+		return new Resource<>(game,
+				linkTo(methodOn(GameController.class).playMove(new GameAction(), game.getGameId())).withRel("_self"),
+				linkTo(methodOn(GameController.class).find(game.getGameId())).withRel("get_game"));
 	}
 
 	@DeleteMapping("/delete/{id}")
-	@ApiOperation(value = "find and delete stored game by given id", response = void.class)
-	ResponseEntity<HttpStatus> deletGame(@PathVariable Long id) {
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	@ApiOperation(value = "find and delete stored game by given id", response = ResponseEntity.class)
+	Resource<HttpStatus> deletGame(@PathVariable Long id) {
 		gameProviderService.deleteGameId(id);
-		return new ResponseEntity<HttpStatus> (HttpStatus.ACCEPTED);
+		return new Resource<> (HttpStatus.ACCEPTED);
 	}
 
 }
